6fcfaf3beb4e8cfb21fe2253341c24c032540d48
ming luo <itestmycode@gmail.com>
2019-07-18 19:06:17 +0000

    remove eunit and unused app

--- applications/functions/test/functions_tests.erl
+++ applications/functions/test/functions_tests.erl
@@ -4,7 +4,4 @@
 -include("functions.hrl").
 
 func_init_module_test() ->
-    lager:start(),
-    ?assertEqual(ok, functions_init:init_modules()),
-    Modules = functions_init:existing_modules(),
-    ?assertEqual([functions_tests], Modules).
+    ok.
